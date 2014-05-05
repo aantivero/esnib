0e266ed0cc3708b86a5078f403ba65ac70ef4a35
Alejandro Antivero <alejandro.antivero@gmail.com>
2014-05-05 00:07:10 -0300

    Se modifico la barra de navegación

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '10.10.10.30',
+        hostname: '192.168.0.102',
         livereload: 35729
       },
       livereload: {
